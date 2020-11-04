98ef7d63157fe2e17b068bb3d502d7e75345af3f
Christofer Bäcklin <christofer.backlin@formulate.app>
2020-11-04 09:00:00 +0100

    separate example 4 into gopath and vendored

--- 4_vendored/test4_vendored.pb.go
+++ 4_vendored/test4_vendored.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // 	protoc-gen-go v1.25.0
 // 	protoc        v3.12.4
-// source: test4.proto
+// source: test4_vendored.proto
 
 package test4
 
@@ -37,7 +37,7 @@ type M4 struct {
 func (x *M4) Reset() {
 	*x = M4{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_test4_proto_msgTypes[0]
+		mi := &file_test4_vendored_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -50,7 +50,7 @@ func (x *M4) String() string {
 func (*M4) ProtoMessage() {}
 
 func (x *M4) ProtoReflect() protoreflect.Message {
-	mi := &file_test4_proto_msgTypes[0]
+	mi := &file_test4_vendored_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -63,7 +63,7 @@ func (x *M4) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use M4.ProtoReflect.Descriptor instead.
 func (*M4) Descriptor() ([]byte, []int) {
-	return file_test4_proto_rawDescGZIP(), []int{0}
+	return file_test4_vendored_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *M4) GetDate() *date.Date {
@@ -73,36 +73,36 @@ func (x *M4) GetDate() *date.Date {
 	return nil
 }
 
-var File_test4_proto protoreflect.FileDescriptor
+var File_test4_vendored_proto protoreflect.FileDescriptor
 
-var file_test4_proto_rawDesc = []byte{
-	0x0a, 0x0b, 0x74, 0x65, 0x73, 0x74, 0x34, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x05, 0x74,
-	0x65, 0x73, 0x74, 0x34, 0x1a, 0x16, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2f, 0x74, 0x79, 0x70,
-	0x65, 0x2f, 0x64, 0x61, 0x74, 0x65, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0x2b, 0x0a, 0x02,
-	0x4d, 0x34, 0x12, 0x25, 0x0a, 0x04, 0x64, 0x61, 0x74, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b,
-	0x32, 0x11, 0x2e, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2e, 0x74, 0x79, 0x70, 0x65, 0x2e, 0x44,
-	0x61, 0x74, 0x65, 0x52, 0x04, 0x64, 0x61, 0x74, 0x65, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f,
-	0x33,
+var file_test4_vendored_proto_rawDesc = []byte{
+	0x0a, 0x14, 0x74, 0x65, 0x73, 0x74, 0x34, 0x5f, 0x76, 0x65, 0x6e, 0x64, 0x6f, 0x72, 0x65, 0x64,
+	0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x05, 0x74, 0x65, 0x73, 0x74, 0x34, 0x1a, 0x16, 0x67,
+	0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2f, 0x74, 0x79, 0x70, 0x65, 0x2f, 0x64, 0x61, 0x74, 0x65, 0x2e,
+	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0x2b, 0x0a, 0x02, 0x4d, 0x34, 0x12, 0x25, 0x0a, 0x04, 0x64,
+	0x61, 0x74, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x11, 0x2e, 0x67, 0x6f, 0x6f, 0x67,
+	0x6c, 0x65, 0x2e, 0x74, 0x79, 0x70, 0x65, 0x2e, 0x44, 0x61, 0x74, 0x65, 0x52, 0x04, 0x64, 0x61,
+	0x74, 0x65, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_test4_proto_rawDescOnce sync.Once
-	file_test4_proto_rawDescData = file_test4_proto_rawDesc
+	file_test4_vendored_proto_rawDescOnce sync.Once
+	file_test4_vendored_proto_rawDescData = file_test4_vendored_proto_rawDesc
 )
 
-func file_test4_proto_rawDescGZIP() []byte {
-	file_test4_proto_rawDescOnce.Do(func() {
-		file_test4_proto_rawDescData = protoimpl.X.CompressGZIP(file_test4_proto_rawDescData)
+func file_test4_vendored_proto_rawDescGZIP() []byte {
+	file_test4_vendored_proto_rawDescOnce.Do(func() {
+		file_test4_vendored_proto_rawDescData = protoimpl.X.CompressGZIP(file_test4_vendored_proto_rawDescData)
 	})
-	return file_test4_proto_rawDescData
+	return file_test4_vendored_proto_rawDescData
 }
 
-var file_test4_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
-var file_test4_proto_goTypes = []interface{}{
+var file_test4_vendored_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_test4_vendored_proto_goTypes = []interface{}{
 	(*M4)(nil),        // 0: test4.M4
 	(*date.Date)(nil), // 1: google.type.Date
 }
-var file_test4_proto_depIdxs = []int32{
+var file_test4_vendored_proto_depIdxs = []int32{
 	1, // 0: test4.M4.date:type_name -> google.type.Date
 	1, // [1:1] is the sub-list for method output_type
 	1, // [1:1] is the sub-list for method input_type
@@ -111,13 +111,13 @@ var file_test4_proto_depIdxs = []int32{
 	0, // [0:1] is the sub-list for field type_name
 }
 
-func init() { file_test4_proto_init() }
-func file_test4_proto_init() {
-	if File_test4_proto != nil {
+func init() { file_test4_vendored_proto_init() }
+func file_test4_vendored_proto_init() {
+	if File_test4_vendored_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_test4_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_test4_vendored_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*M4); i {
 			case 0:
 				return &v.state
@@ -134,18 +134,18 @@ func file_test4_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_test4_proto_rawDesc,
+			RawDescriptor: file_test4_vendored_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_test4_proto_goTypes,
-		DependencyIndexes: file_test4_proto_depIdxs,
-		MessageInfos:      file_test4_proto_msgTypes,
+		GoTypes:           file_test4_vendored_proto_goTypes,
+		DependencyIndexes: file_test4_vendored_proto_depIdxs,
+		MessageInfos:      file_test4_vendored_proto_msgTypes,
 	}.Build()
-	File_test4_proto = out.File
-	file_test4_proto_rawDesc = nil
-	file_test4_proto_goTypes = nil
-	file_test4_proto_depIdxs = nil
+	File_test4_vendored_proto = out.File
+	file_test4_vendored_proto_rawDesc = nil
+	file_test4_vendored_proto_goTypes = nil
+	file_test4_vendored_proto_depIdxs = nil
 }
